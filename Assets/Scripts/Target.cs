ba6dccd1b007ba91d70bb37cf0cbf63f6f65c28c
nlash-fitch <nlash@student.fitchburgstate.edu>
2025-04-16 18:39:46 -0400

    targets are destroyed when game ends

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,16 +6,22 @@ public class Target : MonoBehaviour
 {
     public int speed = 5;
     public GameObject me;
+    public GameObject manager;
+    public GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.Find("Game manager");
+        gameManager = manager.GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.down*Time.deltaTime*speed);
+        if (!gameManager.startGame) {
+            Destroy(me);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
